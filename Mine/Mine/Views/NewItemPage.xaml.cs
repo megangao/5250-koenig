0e48fb43dc9a348938a8535cd548aaf7e5dc9fa6
Megan Gao <36392219+megangao@users.noreply.github.com>
2021-01-26 02:03:39 -0800

    Added Stepper for Value Attribute on NewItemPage

--- Mine/Mine/Views/NewItemPage.xaml.cs
+++ Mine/Mine/Views/NewItemPage.xaml.cs
@@ -38,5 +38,15 @@ namespace Mine.Views
         {
             await Navigation.PopModalAsync();
         }
+
+        /// <summary>
+        /// Update the Display Value when the Stepper Changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
