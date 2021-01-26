5aa614f4e65e612df348796661a8aaee23b50b47
Megan Gao <36392219+megangao@users.noreply.github.com>
2021-01-26 05:49:31 -0800

    Added DeleteItem and CancelItem toolbar events to ItemDeletePage.xaml, ItemDeletePage.xaml.cs

--- Mine/Mine/Views/Items/ItemDeletePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemDeletePage.xaml.cs
@@ -35,5 +35,26 @@ namespace Mine.Views
             viewModel = new ItemReadViewModel(item);
             BindingContext = viewModel;
         }
+
+        /// <summary>
+        /// Cancel the Page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void CancelItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Confirm the Delete for this item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void DeleteItem_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "DeleteItem", viewModel.Item);
+            await Navigation.PopModalAsync();
+        }
     }
 }
